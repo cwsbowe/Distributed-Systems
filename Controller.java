d86ea05c138fec3af201dc3b2d74afa1aae6db55
cwsbowe <charliewsbowe@gmail.com>
2022-05-16 23:41:03 +0100

    5th

--- Controller.java
+++ Controller.java
@@ -4,57 +4,68 @@ import java.util.ArrayList;
 
 
 public class Controller {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private ArrayList<Dstore> dstores;
-    private int _numOfClients = 0;
+    private Socket contSocket;
     private InputStream in;
     private OutputStream out;
-
-    public void start (int cport, int R, int timeout, int rebalance_period) {
-        dstores = new ArrayList<Dstore>();
-        for (int i=0; i<R; i++) {
-            dstores.add(new Dstore())
-        }
-    }
-
-    private void run() {
-        try {
-            Socket s;
-            while (true) {
-                s = serverSocket.accept();
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    public void stop () {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
-
-    public static void main (String[] args) throws Exception{
-        Controller cont = new Controller();
-        try {
-            cont.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-        } catch (Exception e) {
-            System.out.println("Error, not enough arguments");
-        }
-    }
-
-
-    private class contThread extends Thread {
-        private Socket _clientSocket;
-        private int port;
-
-        contThread(Socket client) throws IOException {
-            _clientSocket = client;
-            
-        }
-
-    }
-
 }
+
+
+// public class Controller {
+//     private ServerSocket serverSocket;
+//     private Socket clientSocket;
+//     private ArrayList<Dstore> dstores;
+//     private int _numOfClients = 0;
+//     private InputStream in;
+//     private OutputStream out;
+
+//     public void start (int cport, int R, int timeout, int rebalance_period) {
+//         dstores = new ArrayList<Dstore>();
+//         for (int i=0; i<R; i++) {
+//             dstores.add(new Dstore());
+//         }
+//     }
+
+//     private void run() {
+//         try {
+//             Socket s;
+//             while (true) {
+//                 s = serverSocket.accept();
+//             }
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+//     }
+
+//     public void stop () {
+//         try {
+//             in.close();
+//             out.close();
+//             clientSocket.close();
+//             serverSocket.close();
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+//     }
+
+//     public static void main (String[] args) throws Exception{
+//         Controller cont = new Controller();
+//         try {
+//             cont.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+//         } catch (Exception e) {
+//             System.out.println("Error, not enough arguments");
+//         }
+//     }
+
+
+//     private class contThread extends Thread {
+//         private Socket _clientSocket;
+//         private int port;
+
+//         contThread(Socket client) throws IOException {
+//             _clientSocket = client;
+            
+//         }
+
+//     }
+
+// }
--- Dstore.java
+++ Dstore.java
@@ -3,5 +3,65 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Dstore {
-    
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private Socket controllerSocket;
+    private BufferedReader fromClient;
+    private PrintWriter toClient;
+    private BufferedReader fromCont;
+    private PrintWriter toCont;
+    private String[] fstLine;
+    private Boolean sto;
+    private ArrayList<String> temps;
+    private File file;
+
+
+    public void start(int port, int cport, int timeout, String file_folder) {
+        sto = false;
+        temps = new ArrayList<String>();
+        try {
+            serverSocket = new ServerSocket(port); //connects to client
+            controllerSocket = new Socket(InetAddress.getLocalHost(), cport); //connects to controller
+            while (true) {
+                socket = serverSocket.accept(); //waits for client to send something
+                fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                toClient = new PrintWriter(socket.getOutputStream(), true);
+                if (sto) {
+                    file.createNewFile();
+                    FileWriter w = new FileWriter(file);
+                    String line;
+                    while ((line = fromClient.readLine()) != null) {
+                        w.write(line);
+                    }
+                    w.close();
+                    toCont = new PrintWriter(controllerSocket.getOutputStream(), true);
+                    toCont.println("STORE_ACK " + temps.get(0));
+                } else {
+                    fstLine = fromClient.readLine().split(" ");
+                    if (fstLine[0].equals("STORE")) {
+                        file = new File(fstLine[1]);
+                        if (file.exists()) {
+                            toCont.println("ERROR_FILE_ALREADY_EXISTS"); //controller sends this to client
+                        } else {
+                            toClient.println("ACK");
+                            sto = true;
+                            temps.add(fstLine[1]);
+                            temps.add(fstLine[2]);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Dstore ds = new Dstore();
+        try {
+            ds.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
